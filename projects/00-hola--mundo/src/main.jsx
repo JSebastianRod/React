26a771a96f35ae6e86c7a80ce0a460dabaeac896
JSebastianRod <sebastianrodriguez019@gmail.com>
2024-08-12 23:37:21 -0500

    00 Completo

--- projects/00-hola--mundo/src/main.jsx
+++ projects/00-hola--mundo/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const Button = ({text}) => {
+  return (
+    <button>
+      {text}
+    </button>
+   
+  )
+}
+
+root.render(
+  <React.Fragment>
+    <Button text="Button 1" />
+    <Button text="Button 2" />
+    <Button text="Button 3" />
+  </React.Fragment>
 )
+
+
