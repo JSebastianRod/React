2fbc5dbb1a5c0a9761ba960a792b10eff4ad6bdd
JSebastianRod <sebastianrodriguez019@gmail.com>
2024-08-17 01:26:52 -0500

    cat app

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto', {enabled})
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+    
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        
+      }}
+      />
+      <h3>Proyecto 3</h3>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar': 'Activar'} seguir puntero</button>
+    </main>
+  )
+}
+
+export default App
